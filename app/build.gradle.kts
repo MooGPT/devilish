c3acf6bacdea76c9641ae712f6b313210ea86588
SimSimi Education Company <sashanurmi@hotmail.com>
2024-10-01 01:08:16 +0300

    Added project App level of the upper

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath(kotlin("gradle-plugin", version = "1.7.10"))
+        implementation("androidx.core:core-ktx:1.7.0")
+        implementation("androidx.appcompat:appcompat:1.4.1")
+        implementation("com.google.android.material:material:1.5.0")
+        implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+        testImplementation("junit:junit:4.13.2")
+        androidTestImplementation("androidx.test.ext:junit:1.1.3")
+       androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+        plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+
+    android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.ismaker.devilish"
+        minSdk = 21
+        targetSdk = 23
+        versionCode = 1
+        versionName = "1.0.0"
+        
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
+
+buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
