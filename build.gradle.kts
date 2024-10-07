5dec812e977b3f6658c36de3f74b85c0b5e440bd
SimSimi Education Company <sashanurmi@hotmail.com>
2024-10-08 01:40:19 +0300

    Release 1.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId "com.ismaker.devilish"
         minSdkVersion 21
         targetSdkVersion 22
-        versionCode 3
-        versionName "1.0.2"
+        versionCode 4
+        versionName "1.0.3"
         testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
