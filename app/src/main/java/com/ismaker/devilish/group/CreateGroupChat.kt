7c52df6c7064affdcd84d365beea80dbb53fb47b
SimSimi Education Company <sashanurmi@hotmail.com>
2024-10-16 17:28:27 +0300

    Added license of CC-BY-4.0 for Kotlin

--- app/src/main/java/com/ismaker/devilish/group/CreateGroupChat.kt
+++ app/src/main/java/com/ismaker/devilish/group/CreateGroupChat.kt
@@ -1,3 +1,18 @@
+/*
+* Copyright (C) 2024 Matthias Urhahn, and MooGPT Inc.
+* 
+* Licensed under the CC-BY, version 4.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*      https://creativecommons.org/licenses/by-sa/4.0/deed.ru
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package com.ismaker.devilish.group
 
 import android.os.Bundle
